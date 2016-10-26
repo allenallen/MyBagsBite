e5a6621275b3b9b3c8eb95bdc49472842d0407e2
Allen <eaarcenal@inovotek.com>
2016-10-26 18:53:56 +0800

    homepage

---
+++ app/src/main/java/com/bb/mybagsbite/Views/HomeView.java
@@ -0,0 +1,11 @@
+package com.bb.mybagsbite.Views;
+
+import android.widget.Button;
+
+/**
+ * Created by eaarcenal on 10/26/16.
+ */
+
+public interface HomeView {
+    void userOptions(Button... buttons);
+}
